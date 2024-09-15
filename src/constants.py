48a4d6d06c98f0e27a9e6e5ef6f8dfef91eb1896
Snipy7374 <100313469+Snipy7374@users.noreply.github.com>
2024-09-15 22:37:32 +0200

    remove print and run pre-commit

--- src/constants.py
+++ src/constants.py
@@ -6,7 +6,6 @@ SCREEN_HEIGHT = 600
 SCREEN_TITLE = ""
 # Path to the game's root directory
 ROOT_DIR = Path(__file__).parent.parent
-print(ROOT_DIR.absolute())
 
 # Path to the game's assets directory
 ASSETS_DIR = ROOT_DIR / "assets"
