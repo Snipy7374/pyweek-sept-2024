93ea4d88a2fb7d2d9bb31e703f48394d6093ff9b
Snipy7374 <100313469+Snipy7374@users.noreply.github.com>
2024-09-15 23:03:27 +0200

    fix: change ROOT_DIR constant (#3)

--- src/constants.py
+++ src/constants.py
@@ -5,7 +5,13 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = ""
 # Path to the game's root directory
-ROOT_DIR = Path(__file__).parent.parent
+_path = Path("src")
+try:
+    ROOT_DIR = _path.resolve(strict=True)
+except FileNotFoundError as e:
+    raise RuntimeError(
+        "Root dir not found. Perhaps try to run the project from root and not inside src."
+    ) from e
 
 # Path to the game's assets directory
 ASSETS_DIR = ROOT_DIR / "assets"
