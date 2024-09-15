2ade1f1cb747a00f179a4827f76a0e655b1966ce
Abhishek Kashyap <abhik5567@gmail.com>
2024-09-16 00:02:48 +0530

    feat : add the basic files for the game.

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = ""
+# Path to the game's root directory
+ROOT_DIR = Path(__file__).parent.parent
+print(ROOT_DIR.absolute())
+
+# Path to the game's assets directory
+ASSETS_DIR = ROOT_DIR / "assets"
---
+++ src/main.py
@@ -0,0 +1,10 @@
+from window import Window
+
+
+def main():
+    window = Window()
+    window.run()
+
+
+if __name__ == "__main__":
+    main()
